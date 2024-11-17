af41b373c2fd7852ecaec38d6a79e966afbfac20
aarmst24 <142625404+aarmst24@users.noreply.github.com>
2024-11-17 16:01:49 -0500

    Adding initial files

--- Project/config.py
+++
@@ -1,7 +0,0 @@
-
-
-OPENAI_API_KEY = "OPENAI_KEY"
-GPT3_MODEL_NAME = "text-davinci-002"
-DALLE2_MODEL_NAME = "image-alpha-001"
-CODEX_MODEL_NAME = "davinci-codex"
-CHATGPT_MODEL_NAME = "chatbot-beta"
